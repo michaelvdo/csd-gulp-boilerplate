995aaf9e0024d8742a9605fecf8bc395934a4d33
Michael van den Oudenalder <michael.vanden.oudenalder@efocus.nl>
2016-01-20 09:43:58 +0100

    added IIFE to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,113 +1,117 @@
-'use strict';
+(function() {
 
-// requires
-var gulp            = require('gulp'),
-    autoprefixer    = require('gulp-autoprefixer'),
-    browserSync     = require('browser-sync').create(),
-    inject          = require('gulp-inject'),
-    mainBowerFiles  = require('main-bower-files')
-    normalize       = require('node-normalize-scss'),
-    sass            = require('gulp-sass'),
-    sourcemaps      = require('gulp-sourcemaps');
+  'use strict';
 
-//
-// directory variables
-//
+  // requires
+  var gulp            = require('gulp'),
+      autoprefixer    = require('gulp-autoprefixer'),
+      browserSync     = require('browser-sync').create(),
+      inject          = require('gulp-inject'),
+      mainBowerFiles  = require('main-bower-files')
+      normalize       = require('node-normalize-scss'),
+      sass            = require('gulp-sass'),
+      sourcemaps      = require('gulp-sourcemaps');
 
-// project name
-var projectName = 'project_gulp';
+  //
+  // directory variables
+  //
 
-// serve folder
-var baseDir = 'dist/'+ projectName + '/' + projectName + '.ui.apps/src/main/content/jcr_root/etc/designs/' + projectName;
+  // project name
+  var projectName = 'project_gulp';
 
-// dist directories
-var htmlDistFiles   = baseDir + '/frontend/',
-    cssDistFiles    = baseDir + '/clientlibs_base/css/',
-    jsDistFiles     = baseDir + '/clientlibs_body/js/',
-    fontsDistFiles  = baseDir + '/clientlibs_base/fonts/',
-    imgDistFiles    = baseDir + '/clientlibs_base/img/';
+  // serve folder
+  var baseDir = 'dist/'+ projectName + '/' + projectName + '.ui.apps/src/main/content/jcr_root/etc/designs/' + projectName;
 
-// source directories
-var htmlFiles = 'app/html/*.html',
-    sassFiles = 'app/styles/**/*.scss',
-    jsFiles   = 'app/js/**/*.js';
+  // dist directories
+  var htmlDistFiles   = baseDir + '/frontend/',
+      cssDistFiles    = baseDir + '/clientlibs_base/css/',
+      jsDistFiles     = baseDir + '/clientlibs_body/js/',
+      fontsDistFiles  = baseDir + '/clientlibs_base/fonts/',
+      imgDistFiles    = baseDir + '/clientlibs_base/img/';
 
-//
-// gulp tasks
-//
+  // source directories
+  var htmlFiles = 'app/html/*.html',
+      sassFiles = 'app/styles/**/*.scss',
+      jsFiles   = 'app/js/**/*.js';
 
-// gulp static server task (start server on port 8080)
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: baseDir,
-      directory: true
-    },
-    port: 8080,
-    notify: false
+  //
+  // gulp tasks
+  //
+
+  // gulp static server task (start server on port 8080)
+  gulp.task('browser-sync', function() {
+    browserSync.init({
+      server: {
+        baseDir: baseDir,
+        directory: true
+      },
+      port: 8080,
+      notify: false
+    });
   });
-});
 
-// gulp html task (move html files to dist and refresh browser)
-gulp.task('html', function() {
-  return gulp.src(htmlFiles)
-  .pipe(gulp.dest(htmlDistFiles))
-  .pipe(browserSync.stream());
-});
+  // gulp html task (move html files to dist and refresh browser)
+  gulp.task('html', function() {
+    return gulp.src(htmlFiles)
+    .pipe(gulp.dest(htmlDistFiles))
+    .pipe(browserSync.stream());
+  });
 
-// gulp sass task (process sass files, add sourcemap and prefixes, refresh browser)
-gulp.task('sass', function() {
-  return gulp.src(sassFiles)
-  .pipe(sourcemaps.init())
-  .pipe(sass({
-    includePaths: normalize.includePaths // add normalize to compiled css
-  }).on('error', sass.logError))
-  .pipe(autoprefixer({
-    browsers: ['not ie <= 8', 'last 3 versions']
-  }))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest(cssDistFiles))
-  .pipe(browserSync.stream());
-});
+  // gulp sass task (process sass files, add sourcemap and prefixes, refresh browser)
+  gulp.task('sass', function() {
+    return gulp.src(sassFiles)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: normalize.includePaths // add normalize to compiled css
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['not ie <= 8', 'last 3 versions']
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(cssDistFiles))
+    .pipe(browserSync.stream());
+  });
 
-// gulp js task (move js files to dist and refresh browser)
-gulp.task('js', function() {
-  return gulp.src(jsFiles)
-  .pipe(gulp.dest(jsDistFiles))
-  .pipe(browserSync.stream());
-});
+  // gulp js task (move js files to dist and refresh browser)
+  gulp.task('js', function() {
+    return gulp.src(jsFiles)
+    .pipe(gulp.dest(jsDistFiles))
+    .pipe(browserSync.stream());
+  });
 
-// gulp inject:libs task (inject link/script elements in dist html files)
-gulp.task('inject:libs', function() {
-  return gulp.src(htmlFiles)
-  .pipe(
-    inject(
-      gulp.src(
-        [cssDistFiles + '*.css', jsDistFiles + '*.js'],
-        {read: false}
-      ),
-      {
-      relative: false,
-      ignorePath: baseDir,
-      addPrefix: '..',
-      addRootSlash: false
-      }
+  // gulp inject:libs task (inject link/script elements in dist html files)
+  gulp.task('inject:libs', function() {
+    return gulp.src(htmlFiles)
+    .pipe(
+      inject(
+        gulp.src(
+          [cssDistFiles + '*.css', jsDistFiles + '*.js'],
+          {read: false}
+        ),
+        {
+        relative: false,
+        ignorePath: baseDir,
+        addPrefix: '..',
+        addRootSlash: false
+        }
+      )
     )
-  )
-  .pipe(
-    gulp.dest(htmlDistFiles)
-  );
-});
+    .pipe(
+      gulp.dest(htmlDistFiles)
+    );
+  });
 
-// gulp serve task (setup server, watch for file changes)
-gulp.task('serve', ['sass', 'browser-sync'], function() {
-  gulp.watch( htmlFiles, ['html', 'inject:libs']);
-  gulp.watch( sassFiles, ['sass']);
-  gulp.watch( jsFiles, ['js']);
-});
+  // gulp serve task (setup server, watch for file changes)
+  gulp.task('serve', ['sass', 'browser-sync'], function() {
+    gulp.watch( htmlFiles, ['html', 'inject:libs']);
+    gulp.watch( sassFiles, ['sass']);
+    gulp.watch( jsFiles, ['js']);
+  });
 
-// gulp default task
-gulp.task('default', ['serve']);
+  // gulp default task
+  gulp.task('default', ['serve']);
 
-// gulp init task (run once on project init for file/folder creation)
-gulp.task('init', ['html', 'sass', 'js']);
+  // gulp init task (run once on project init for file/folder creation)
+  gulp.task('init', ['html', 'sass', 'js']);
+
+})();
